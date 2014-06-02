5645a92d8834dd5e8a22954d250e96b455563163
Hung Le <lecaoquochung@gmail.com>
2014-06-02 16:49:32 +0900

    [BlogX] Template

---
+++ x10-bootstrap/js/custom.js
@@ -0,0 +1,38 @@
+/*
+* Author: lecaoquochung
+* Template: X10-Satelite
+* Version: 1.0
+* Email: lecaoquochung@gmail.com
+*/
+
+$(document).ready(function() {
+        
+	$('#sidebar').affix({
+		offset: {
+        	top: 235
+      	}
+	});
+
+	var $body   = $(document.body);
+	var navHeight = $('.navbar').outerHeight(true) + 10;
+
+	$body.scrollspy({
+		target: '#leftCol',
+		offset: navHeight
+	});
+
+	/* smooth scrolling sections */
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top - 50
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	});
+        
+});
