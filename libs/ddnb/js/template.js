095ef9472ab2e411a0dab907b9d7680872eb6b74
hungle <lecaoquochung@gmail.com>
2014-07-30 15:59:40 +0900

    testcode admin css

---
+++ libs/ddnb/js/template.js
@@ -0,0 +1,10 @@
+/*------------------------------------------------------------------------------------------------------------
+/**
+ * template js
+ *
+ * @package		template.js
+ * @author		ddnb_admin <admin@ddnb.info>
+ * @license		http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link		http://lecaoquochung.github.io/ddnb.template/libs/ddnb/js/template.js
+ * @created		2014-07
+-------------------------------------------------------------------------------------------------------------*/
